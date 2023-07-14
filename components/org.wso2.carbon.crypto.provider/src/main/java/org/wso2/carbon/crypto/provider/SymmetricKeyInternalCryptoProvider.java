b59069f4c3e48ae0d080917e865c9c8c3cabdc69
kaya <kayathirikaya33@gmail.com>
2023-07-14 12:01:42 +0530

    Fix comments

--- components/org.wso2.carbon.crypto.provider/src/main/java/org/wso2/carbon/crypto/provider/SymmetricKeyInternalCryptoProvider.java
+++ components/org.wso2.carbon.crypto.provider/src/main/java/org/wso2/carbon/crypto/provider/SymmetricKeyInternalCryptoProvider.java
@@ -159,6 +159,7 @@ public class SymmetricKeyInternalCryptoProvider implements InternalCryptoProvide
      * @param ciphertext                The ciphertext to be decrypted.
      * @param algorithm                 The encryption / decryption algorithm
      * @param javaSecurityAPIProvider   The Java Security Provider.
+     * @param params                    The parameters required for the decryption operation.
      * @return The cleartext
      * @throws CryptoException If something unexpected happens during the decryption operation.
      */
